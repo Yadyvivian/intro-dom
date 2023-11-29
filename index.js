5eeb891f4a54f917bf79c748d531ac5bf2bdb3aa
Yadyvivian <148440973+Yadyvivian@users.noreply.github.com>
2023-11-29 12:42:30 +0100

    change in two and new branch

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function change() {
     // your code goes here
+
+    var titleById = document.getElementById('titleId');
+    var textById = titleById.textContent;
+
+    // Mostrar el contenido de texto en la consola
+    
+    console.log('Texto por ID ("titleId"): ' + textById);
 }
